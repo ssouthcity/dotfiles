192344b893081b956122ed90a385f4792bb74349
Stian Sørby <51554341+ssouthcity@users.noreply.github.com>
2024-01-01 17:02:41 +0100

    safe format check

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -39,7 +39,9 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_create_autocmd("BufWritePre", {
     buffer = bufnr,
     callback = function()
-      vim.lsp.buf.format()
+      if client.server_capabilities.documentFormattingProvider then
+        vim.lsp.buf.format({ bufnr = bufnr })
+      end
     end
   })
 end
